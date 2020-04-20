70e34d9be24ce544a785d2b47af2ae833a491c71
Blaise Frederick <blaise.frederick@gmail.com>
2020-04-20 10:21:39 -0400

    Update _gittag.py

--- grader/_gittag.py
+++ grader/_gittag.py
@@ -1,4 +1,4 @@
 
 # This file is originally generated from Git information by running 'setup.py
 # install'. Distribution tarballs contain a pre-generated copy of this file.
-__gittag__ = '5417a92-dirty'
+__gittag__ = 'b7ac60a'
