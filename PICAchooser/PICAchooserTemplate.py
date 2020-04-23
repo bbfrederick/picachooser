c287740c02344a8e4ffef5d574c156965c4fd093
Blaise Frederick <blaise.frederick@gmail.com>
2020-04-23 14:39:29 -0400

    Allow for smaller screen sizes

--- PICAchooser/PICAchooserTemplate.py
+++ PICAchooser/PICAchooserTemplate.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'grader/aromaviewTemplate.ui'
+# Form implementation generated from reading ui file 'PICAchooser/PICAchooserTemplate.ui'
 #
 # Created by: PyQt5 UI code generator 5.12.3
 #
@@ -13,7 +13,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1648, 786)
+        MainWindow.resize(1746, 839)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
@@ -30,7 +30,7 @@ class Ui_MainWindow(object):
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.translation_graphicsView.sizePolicy().hasHeightForWidth())
         self.translation_graphicsView.setSizePolicy(sizePolicy)
-        self.translation_graphicsView.setMinimumSize(QtCore.QSize(610, 100))
+        self.translation_graphicsView.setMinimumSize(QtCore.QSize(400, 100))
         self.translation_graphicsView.setMaximumSize(QtCore.QSize(610, 1000))
         self.translation_graphicsView.setSizeIncrement(QtCore.QSize(1, 1))
         self.translation_graphicsView.setObjectName("translation_graphicsView")
@@ -42,7 +42,7 @@ class Ui_MainWindow(object):
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.rotation_graphicsView.sizePolicy().hasHeightForWidth())
         self.rotation_graphicsView.setSizePolicy(sizePolicy)
-        self.rotation_graphicsView.setMinimumSize(QtCore.QSize(610, 100))
+        self.rotation_graphicsView.setMinimumSize(QtCore.QSize(400, 100))
         self.rotation_graphicsView.setMaximumSize(QtCore.QSize(610, 1000))
         self.rotation_graphicsView.setSizeIncrement(QtCore.QSize(1, 1))
         self.rotation_graphicsView.setObjectName("rotation_graphicsView")
@@ -54,7 +54,7 @@ class Ui_MainWindow(object):
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.timecourse_graphicsView.sizePolicy().hasHeightForWidth())
         self.timecourse_graphicsView.setSizePolicy(sizePolicy)
-        self.timecourse_graphicsView.setMinimumSize(QtCore.QSize(610, 100))
+        self.timecourse_graphicsView.setMinimumSize(QtCore.QSize(400, 100))
         self.timecourse_graphicsView.setMaximumSize(QtCore.QSize(610, 1000))
         self.timecourse_graphicsView.setSizeIncrement(QtCore.QSize(1, 1))
         self.timecourse_graphicsView.setObjectName("timecourse_graphicsView")
@@ -66,7 +66,7 @@ class Ui_MainWindow(object):
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.spectrum_graphicsView.sizePolicy().hasHeightForWidth())
         self.spectrum_graphicsView.setSizePolicy(sizePolicy)
-        self.spectrum_graphicsView.setMinimumSize(QtCore.QSize(610, 100))
+        self.spectrum_graphicsView.setMinimumSize(QtCore.QSize(400, 100))
         self.spectrum_graphicsView.setMaximumSize(QtCore.QSize(610, 1000))
         self.spectrum_graphicsView.setSizeIncrement(QtCore.QSize(1, 1))
         self.spectrum_graphicsView.setObjectName("spectrum_graphicsView")
@@ -79,15 +79,15 @@ class Ui_MainWindow(object):
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.image_graphicsView.sizePolicy().hasHeightForWidth())
         self.image_graphicsView.setSizePolicy(sizePolicy)
-        self.image_graphicsView.setMinimumSize(QtCore.QSize(1000, 100))
-        self.image_graphicsView.setMaximumSize(QtCore.QSize(2000, 1000))
+        self.image_graphicsView.setMinimumSize(QtCore.QSize(600, 100))
+        self.image_graphicsView.setMaximumSize(QtCore.QSize(2000, 1200))
         self.image_graphicsView.setSizeIncrement(QtCore.QSize(1, 1))
         self.image_graphicsView.setObjectName("image_graphicsView")
         self.horizontalLayout.addWidget(self.image_graphicsView)
         self.gridLayout.addLayout(self.horizontalLayout, 0, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 1648, 22))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1746, 22))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
