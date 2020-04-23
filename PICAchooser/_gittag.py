2a78fae4ff463dffafbb3cd47287ffe869f06054
Blaise Frederick <blaise.frederick@gmail.com>
2020-04-23 13:56:13 -0400

    Save data automatically on close

--- PICAchooser/_gittag.py
+++ PICAchooser/_gittag.py
@@ -1,4 +1,4 @@
 
 # This file is originally generated from Git information by running 'setup.py
 # install'. Distribution tarballs contain a pre-generated copy of this file.
-__gittag__ = '6ebd8a5-dirty'
+__gittag__ = 'bc109ec-dirty'
