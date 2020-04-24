50207861d9e4965700326f9f96cfd30d88e634f0
Blaise Frederick <blaise.frederick@gmail.com>
2020-04-24 12:18:31 -0400

    Update _gittag.py

--- PICAchooser/_gittag.py
+++ PICAchooser/_gittag.py
@@ -1,4 +1,4 @@
 
 # This file is originally generated from Git information by running 'setup.py
 # install'. Distribution tarballs contain a pre-generated copy of this file.
-__gittag__ = 'v1.0.0rc1-dirty'
+__gittag__ = 'v1.0.0rc1-6-gbebe8a1-dirty'
