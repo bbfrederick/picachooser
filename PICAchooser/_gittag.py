4adb88a165354e816823c98bbe38017f7544ffb2
Blaise Frederick <blaise.frederick@gmail.com>
2020-04-24 14:18:35 -0400

    Attempting to force github to recognize case change

--- PICAchooser/_gittag.py
+++
@@ -1,4 +0,0 @@
-
-# This file is originally generated from Git information by running 'setup.py
-# install'. Distribution tarballs contain a pre-generated copy of this file.
-__gittag__ = 'v1.0.0rc1-17-gca9b728-dirty'
