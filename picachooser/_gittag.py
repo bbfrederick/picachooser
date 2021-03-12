71cd2744d7c17ff3b64e22bea7ca2966984bdde5
Blaise deB Frederick <blaise.frederick@gmail.com>
2021-03-12 15:09:22 -0500

    Further additions for group melodic

--- picachooser/_gittag.py
+++ picachooser/_gittag.py
@@ -1,4 +1,4 @@
 
 # This file is originally generated from Git information by running 'setup.py
 # install'. Distribution tarballs contain a pre-generated copy of this file.
-__gittag__ = 'v1.0.2-2-gc62819d-dirty'
+__gittag__ = 'v1.0.2-3-g3ca7297-dirty'
