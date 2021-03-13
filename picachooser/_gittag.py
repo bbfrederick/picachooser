c7a79b337443514d6977a94fa468f55180bec6fa
Blaise Frederick <blaise.frederick@gmail.com>
2021-03-13 12:18:01 -0500

    Reformatted runmode help

--- picachooser/_gittag.py
+++ picachooser/_gittag.py
@@ -1,4 +1,4 @@
 
 # This file is originally generated from Git information by running 'setup.py
 # install'. Distribution tarballs contain a pre-generated copy of this file.
-__gittag__ = 'v1.0.2-3-g3ca7297-dirty'
+__gittag__ = 'v1.0.2-4-g71cd274-dirty'
