a1ceb90280242be2b53f9edb7c8713756e665f47
Blaise Frederick <blaise.frederick@gmail.com>
2021-04-03 14:06:42 -0400

    Reorganized docker files, aligned with rapidtide

---
+++ builddocker.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -ex
+
+# SET THE FOLLOWING VARIABLES
+# docker hub username
+USERNAME=fredericklab
+# image name
+IMAGE=picachooser
+
+# ensure we're up to date
+git pull
+
+# bump version
+version=`cat VERSION | sed 's/+/ /g' | awk '{print $1}'`
+echo "version: $version"
+
+# run build
+docker build . -t $IMAGE \
+    --build-arg VERSION=$version \
+    --build-arg BUILD_DATE=`date +"%Y%m%dT%H%M%S"` \
+    --build-arg VCS_REF=`git rev-parse HEAD`
+
+# tag it
+docker tag $IMAGE:latest $USERNAME/$IMAGE:latest
+docker tag $IMAGE:latest $USERNAME/$IMAGE:$version
+
+# push it
+docker push $USERNAME/$IMAGE:latest
+docker push $USERNAME/$IMAGE:$version
