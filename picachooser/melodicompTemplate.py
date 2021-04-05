208872d8803f3d623f34a927799026255707df8a
Blaise Frederick <blaise.frederick@gmail.com>
2021-04-05 16:27:26 -0400

    Fixed resizing problem

--- picachooser/melodicompTemplate.py
+++ picachooser/melodicompTemplate.py
@@ -13,35 +13,29 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1750, 846)
-        sizePolicy = QtWidgets.QSizePolicy(
-            QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding
-        )
+        MainWindow.resize(1750, 826)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
         MainWindow.setSizePolicy(sizePolicy)
-        MainWindow.setMinimumSize(QtCore.QSize(200, 100))
+        MainWindow.setMinimumSize(QtCore.QSize(0, 100))
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(-30, 0, 1771, 781))
-        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
-        self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
+        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName("gridLayout")
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setSizeConstraint(QtWidgets.QLayout.SetNoConstraint)
-        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
         self.horizontalLayout.setObjectName("horizontalLayout")
         self.verticalLayout = QtWidgets.QVBoxLayout()
         self.verticalLayout.setObjectName("verticalLayout")
-        self.pane1_label = QtWidgets.QLabel(self.horizontalLayoutWidget)
+        self.pane1_label = QtWidgets.QLabel(self.centralwidget)
         self.pane1_label.setAlignment(QtCore.Qt.AlignCenter)
         self.pane1_label.setObjectName("pane1_label")
         self.verticalLayout.addWidget(self.pane1_label)
-        self.image_graphicsView_1 = GraphicsLayoutWidget(self.horizontalLayoutWidget)
+        self.image_graphicsView_1 = GraphicsLayoutWidget(self.centralwidget)
         self.image_graphicsView_1.setEnabled(True)
-        sizePolicy = QtWidgets.QSizePolicy(
-            QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding
-        )
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.image_graphicsView_1.sizePolicy().hasHeightForWidth())
@@ -54,15 +48,13 @@ class Ui_MainWindow(object):
         self.horizontalLayout.addLayout(self.verticalLayout)
         self.verticalLayout_2 = QtWidgets.QVBoxLayout()
         self.verticalLayout_2.setObjectName("verticalLayout_2")
-        self.pane2_label = QtWidgets.QLabel(self.horizontalLayoutWidget)
+        self.pane2_label = QtWidgets.QLabel(self.centralwidget)
         self.pane2_label.setAlignment(QtCore.Qt.AlignCenter)
         self.pane2_label.setObjectName("pane2_label")
         self.verticalLayout_2.addWidget(self.pane2_label)
-        self.image_graphicsView_2 = GraphicsLayoutWidget(self.horizontalLayoutWidget)
+        self.image_graphicsView_2 = GraphicsLayoutWidget(self.centralwidget)
         self.image_graphicsView_2.setEnabled(True)
-        sizePolicy = QtWidgets.QSizePolicy(
-            QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding
-        )
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.image_graphicsView_2.sizePolicy().hasHeightForWidth())
@@ -73,6 +65,7 @@ class Ui_MainWindow(object):
         self.image_graphicsView_2.setObjectName("image_graphicsView_2")
         self.verticalLayout_2.addWidget(self.image_graphicsView_2)
         self.horizontalLayout.addLayout(self.verticalLayout_2)
+        self.gridLayout.addLayout(self.horizontalLayout, 0, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 1750, 24))
@@ -88,12 +81,6 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pane1_label.setText(
-            _translate("MainWindow", "Component XX of XX: XX.X% explained var., XX.X% total var.")
-        )
-        self.pane2_label.setText(
-            _translate("MainWindow", "Component XX of XX: XX.X% explained var., XX.X% total var.")
-        )
-
-
+        self.pane1_label.setText(_translate("MainWindow", "Component XX of XX: XX.X% explained var., XX.X% total var."))
+        self.pane2_label.setText(_translate("MainWindow", "Component XX of XX: XX.X% explained var., XX.X% total var."))
 from pyqtgraph import GraphicsLayoutWidget
