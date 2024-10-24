34ef817fbd92be6321e79459e5413e3346844f1d
Blaise deB Frederick <blaise.frederick@gmail.com>
2024-10-24 17:15:58 -0400

    Update builddocker.sh

--- builddocker.sh
+++ builddocker.sh
@@ -16,15 +16,32 @@ version=`cat VERSION | sed 's/+/ /g' | awk '{print $1}'`
 echo "version: $version"
 
 # run build
-docker build . -t $IMAGE \
+docker buildx build . \
+    --platform linux/arm64 \
+    --tag $USERNAME/$IMAGE:latest \
     --build-arg VERSION=$version \
     --build-arg BUILD_DATE=`date +"%Y%m%dT%H%M%S"` \
     --build-arg VCS_REF=`git rev-parse HEAD`
+#    -t $IMAGE \
+#    --tag $USERNAME/$IMAGE:$version \
+
+#docker buildx build . \
+#    --platform linux/amd64 \
+#    --tag $USERNAME/$IMAGE:latest \
+#    --build-arg VERSION=$version \
+#    --build-arg BUILD_DATE=`date +"%Y%m%dT%H%M%S"` \
+#    --build-arg VCS_REF=`git rev-parse HEAD`
+
+#docker build . \
+#    --tag $USERNAME/$IMAGE:latest \
+#    --build-arg VERSION=$version \
+#    --build-arg BUILD_DATE=`date +"%Y%m%dT%H%M%S"` \
+#    --build-arg VCS_REF=`git rev-parse HEAD`
 
 # tag it
-docker tag $IMAGE:latest $USERNAME/$IMAGE:latest
-docker tag $IMAGE:latest $USERNAME/$IMAGE:$version
+#docker tag $IMAGE:latest $USERNAME/$IMAGE:latest
+#docker tag $IMAGE:latest $USERNAME/$IMAGE:$version
 
 # push it
-docker push $USERNAME/$IMAGE:latest
-docker push $USERNAME/$IMAGE:$version
+#docker push $USERNAME/$IMAGE:latest
+#docker push $USERNAME/$IMAGE:$version
